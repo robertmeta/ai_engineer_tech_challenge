ca982ed47260c463f07524da67eddec1857fc308
Robert Melton <robert@robertmelton.com>
2024-10-03 00:53:19 -0400

    Add config parser

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+from functools import cache
+
+from dotenv import dotenv_values
+
+
+@cache
+def get_config():
+    config_dir = os.path.realpath(os.path.dirname(__file__))
+
+    config = {
+        "CONFIG_DIR": config_dir,
+        **dotenv_values(os.path.join(mono_dir, ".env")),
+        # load shared development variables
+        # **dotenv_values(
+        #     os.path.join(src_dir, "..", ".env.secret")
+        # ),  # load sensitive variables
+        **os.environ,  # override loaded values with environment variables
+    }
+
+    if config["DEBUG"].lower() == "true":
+        config["DEBUG"] = True
+    else:
+        config["DEBUG"] = False
+
+    return config
